3a1160c1a306144b3fa7f8c755468ee8e5d6c4b9
glebneshik <117161009+glebneshik@users.noreply.github.com>
2023-09-13 17:57:03 +0500

    Update script.js

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    function textSlceAdd() {
+        var objectVectory = {
+            question: "зачем кирилл ходит в туалет?", // вопрос
+            
+            anwer: "чтобы посидеть в ноутбуке!"  // ответ
+        }
+    }
+    textSlceAdd();
+});
